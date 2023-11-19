3a9aa30a7860c738825f20b96af160ba5d7b6457
minko.naydenov <minko.naydenov@softwaregroup.com>
2023-11-19 14:44:07 +0200

    fix: Link path

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -12,13 +12,13 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Home', href: {pathname: '/dashboard'}, icon: HomeIcon },
   {
     name: 'Invoices',
-    href: '/dashboard/invoices',
+    href: {pathname: '/dashboard/invoices'},
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Customers', href: {pathname: '/dashboard/customers'}, icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
@@ -31,11 +31,11 @@ export default function NavLinks() {
         return (
           <Link
             key={link.name}
-            href={link.href}
+            href={ link.href }
             className={clsx(
               "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
               ,{
-                'bg-sky-100 text-blue-600': pathname === link.href
+                'bg-sky-100 text-blue-600': pathname === link.href.pathname
               }  
             )}
           >
