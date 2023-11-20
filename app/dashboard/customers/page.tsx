981b390b30e87d59b0365b76c2eee599e9d25427
minko.naydenov <minko.naydenov@softwaregroup.com>
2023-11-20 23:21:07 +0200

    feat: add streaming by c page/components

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-    return <p>Customer page</p>
+import { fetchCustomers } from '@/app/lib/data';
+
+export default async function Page() {
+    //await new Promise((resolve) => setTimeout(resolve, 2000));
+    const customers = await fetchCustomers();
+
+    return (
+    <div>
+        <p>Customer page</p>
+        <ul>
+            {customers.map( (item , index)=> <li key={item.id}>{item.name}</li>)}
+        </ul>
+        
+    </div>)
 }
