45040f73dd0b7058774059a579b8cb009be5cb1f
minko.naydenov <minko.naydenov@softwaregroup.com>
2023-12-01 19:42:50 +0200

    fix: type script

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -2,7 +2,7 @@
 
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
-import Link, { LinkProps } from 'next/link';
+import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
@@ -75,7 +75,7 @@ function PaginationNumber({
   position,
 }: {
   page: number | string;
-  href: string | Object | LinkProps<Object>;
+  href: string | Object | RouteImpl;
   position?: 'first' | 'last' | 'middle' | 'single';
   isActive: boolean;
 }) {
@@ -106,7 +106,7 @@ function PaginationArrow({
   direction,
   isDisabled,
 }: {
-  href: string | Object | UrlObject;
+  href: Object;
   direction: 'left' | 'right';
   isDisabled?: boolean;
 }) {
